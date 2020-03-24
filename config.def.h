857e468199420be34ff6fcb5bcb69a46baee98cf
Yiqun Ding <yiqunding@zju.edu.cn>
2020-03-24 23:37:54 +0800

    fix bug in fg/bg, mouse color
    st has the wrong color index

--- config.def.h
+++ config.def.h
@@ -111,6 +111,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+	"#555555",
 };
 
 
@@ -118,10 +119,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
+unsigned int defaultfg = 15;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
